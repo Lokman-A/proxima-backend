111859377553f6594ec4ac8b5235439d3b2ad598
lokman2022 <hmlokman40@gmail.com>
2024-02-22 22:43:22 +0600

    project created with mongoDB

--- routes/projectRoute.js
+++ routes/projectRoute.js
@@ -1,30 +1,41 @@
 const express = require("express");
+const Project = require("../models/projectModel");
 
 // router
 const router = express.Router();
 
 // get all projects
 router.get("/", (req, res) => {
-  res.json({ message: "get all projects" });
+  res.json({ message: " get all projects" });
 });
 
 // get a single project
 router.get("/:id", (req, res) => {
-  res.json({ message: "get a single project" });
+  res.json({ message: " get a single project" });
 });
 
 // post a new project
-router.post("/", (req, res) => {
-  res.json({ message: "post a new project" });
+router.post("/", async (req, res) => {
+  const data = req.body;
+
+  try {
+    const project = await Project.create({
+      ...data,
+    });
+    res.status(200).json(project);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 });
 
 // delete a project
 router.delete("/:id", (req, res) => {
-  res.json({ message: "delete a project" });
+  res.json({ message: " delete a project" });
 });
 
-// fetch a new project
+// patch a new project
 router.patch("/:id", (req, res) => {
-  res.json({ message: "update a projects" });
+  res.json({ message: " patch a new project" });
 });
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,21 +1,25 @@
 require("dotenv").config();
-
 const express = require("express");
-const projectRoutes = require("./routes/projectRoute");
+const router = require("./routes/projectRoute");
+const { default: mongoose } = require("mongoose");
 
 // express app
 const app = express();
-
 // port
 const port = process.env.PORT || 2000;
-
 // middlewares
 app.use(express.json());
-
 // routes
-app.use("/api/projects", projectRoutes);
-
-// listen on port
-app.listen(port, () => {
-  console.log(`listening on ${port}`);
-});
+app.use("/api/projects", router);
+// mongoDB
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    // listen on port
+    app.listen(port, () => {
+      console.log(` listening on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
