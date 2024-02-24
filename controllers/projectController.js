02321135bafbd10cb72bd7a509c9c79aa3550b14
lokman2022 <hmlokman40@gmail.com>
2024-02-24 07:17:02 +0600

    all controller added

---
+++ controllers/projectController.js
@@ -0,0 +1,92 @@
+const { mongoose } = require("mongoose");
+const Project = require("../models/projectModel");
+
+///////////////////////////////////////////////////////////////////////////////////////////
+//get all projects
+///////////////////////////////////////////////////////////////////////////////////////////
+const allProjects = async (req, res) => {
+  const projects = await Project.find({});
+
+  res.status(200).json(projects);
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////
+//get a single project
+////////////////////////////////////////////////////////////////////////////////////////////
+const getSingleProject = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ message: "Invalid project id" });
+  }
+
+  const project = await Project.findById(id);
+
+  if (!project) {
+    return res.status(404).json({ message: "Project not found" });
+  }
+  res.status(200).json(project);
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////
+// post a new project
+////////////////////////////////////////////////////////////////////////////////////////////
+const postProject = async (req, res) => {
+  const data = req.body;
+
+  try {
+    const project = await Project.create({
+      ...data,
+    });
+
+    res.status(200).json(project);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////
+// delete a project
+////////////////////////////////////////////////////////////////////////////////////////////
+const deleteProject = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ error: "Invalid project id" });
+  }
+
+  const project = await Project.findOneAndDelete({ _id: id });
+  if (!project) {
+    return res.status(404).json({ error: "project not found" });
+  }
+  res.status(200).json(project);
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////
+// update a project
+////////////////////////////////////////////////////////////////////////////////////////////
+const updateProject = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ error: "Invalid project ID" });
+  }
+
+  const project = await Project.findOneAndUpdate({ _id: id });
+  if (!project) {
+    return res.status(404).json({ error: "project not found" });
+  }
+  res.status(200).json(project);
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////
+//
+////////////////////////////////////////////////////////////////////////////////////////////
+
+module.exports = {
+  allProjects,
+  postProject,
+  getSingleProject,
+  deleteProject,
+  updateProject,
+};
--- routes/projectRoute.js
+++ routes/projectRoute.js
@@ -1,41 +1,28 @@
 const express = require("express");
-const Project = require("../models/projectModel");
+const {
+  postProject,
+  allProjects,
+  getSingleProject,
+  deleteProject,
+  updateProject,
+} = require("../controllers/projectController");
 
 // router
 const router = express.Router();
 
 // get all projects
-router.get("/", (req, res) => {
-  res.json({ message: " get all projects" });
-});
+router.get("/", allProjects);
 
 // get a single project
-router.get("/:id", (req, res) => {
-  res.json({ message: " get a single project" });
-});
+router.get("/:id", getSingleProject);
 
 // post a new project
-router.post("/", async (req, res) => {
-  const data = req.body;
-
-  try {
-    const project = await Project.create({
-      ...data,
-    });
-    res.status(200).json(project);
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
-});
+router.post("/", postProject);
 
 // delete a project
-router.delete("/:id", (req, res) => {
-  res.json({ message: " delete a project" });
-});
+router.delete("/:id", deleteProject);
 
 // patch a new project
-router.patch("/:id", (req, res) => {
-  res.json({ message: " patch a new project" });
-});
+router.patch("/:id", updateProject);
 
 module.exports = router;
